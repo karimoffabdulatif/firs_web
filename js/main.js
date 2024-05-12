006f6a3d5450e2f6e2bc52e29f15da544ab33028
karimoffabdulatif <karimoff001@gmail.com>
2024-05-12 10:16:49 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+document.getElementById('exit_btn').addEventListener('click', function() {
+    document.getElementById('regis').style.display = 'block';
+    document.getElementById('regis').style.display = 'none';
+});
+
+document.getElementById('buy_btn').addEventListener('click', function() {
+    document.getElementById('regis').style.display = 'none';
+    document.getElementById('regis').style.display = 'block';
+});
